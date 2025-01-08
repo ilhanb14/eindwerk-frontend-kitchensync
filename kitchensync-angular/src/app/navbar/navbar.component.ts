e18632a75de59b37bf863f8cc3c40de3afba76ea
litchitsme <karelsegers@outlook.com>
2025-01-08 17:37:59 +0100

    Added RouterLinkActive to make sure that active links in the dropdown menu are visible due to a different CSS

--- kitchensync-angular/src/app/navbar/navbar.component.ts
+++ kitchensync-angular/src/app/navbar/navbar.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ClickOutsideDirective } from '../click-outside.directive';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, ClickOutsideDirective],
+  imports: [RouterLink, RouterLinkActive, ClickOutsideDirective],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   isDropdownOpen = false;
-
-
 
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
