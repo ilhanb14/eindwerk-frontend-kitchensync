f7b1e1a387a125ec3719e6b089193048e2584229
Merlinruy <ruysen.merlijn@gmail.com>
2025-01-08 17:46:26 +0100

    added calender component

--- kitchensync-angular/src/app/app.component.ts
+++ kitchensync-angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipesComponent } from "./recipes/recipes.component";
+import { CalendarComponent } from './calender/calender.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RecipesComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
--- kitchensync-angular/src/app/app.routes.ts
+++ kitchensync-angular/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { CalendarComponent } from './calender/calender.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'calendar', component: CalendarComponent },];
