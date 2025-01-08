2dbdd2fec0b4a605a5c70c5b3ce6bc96e5f83184
litchitsme <karelsegers@outlook.com>
2025-01-08 17:34:07 +0100

    Added "home" to the path of home so it didn't always remain an active link with the lay-out of an active link

--- kitchensync-angular/src/app/app.routes.ts
+++ kitchensync-angular/src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'recipes', component: RecipesComponent },
     { path: 'calender', component: CalenderComponent },
     { path: 'user', component: UserComponent },
