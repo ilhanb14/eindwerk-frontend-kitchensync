9a5362cb6073a69276e28bf61f1180a1504788c8
Merlinruy <ruysen.merlijn@gmail.com>
2025-01-13 00:47:25 +0100

    added calender imports

--- kitchensync-angular/src/main.ts
+++ kitchensync-angular/src/main.ts
@@ -1,25 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-//import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
-// Import the Calendar module and Date Adapter
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 
 export const appConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimations(),
-  ],
-  imports: [
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory,
-    }),
   ],
 };
 
