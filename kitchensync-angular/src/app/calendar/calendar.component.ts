465dad4daa3fe2b96e0ca112fc2329d834a0126f
Merlinruy <ruysen.merlijn@gmail.com>
2025-01-13 13:31:10 +0100

    added stylesheets for calendar

--- kitchensync-angular/src/app/calendar/calendar.component.ts
+++ kitchensync-angular/src/app/calendar/calendar.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions } from '@fullcalendar/core'; 
+import { CalendarOptions, EventInput } from '@fullcalendar/core'; 
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
 @Component({
     selector: 'app-calendar',
     standalone: true,
     imports: [CommonModule, FullCalendarModule],
     templateUrl: './calendar.component.html',
-    styleUrls: ['./calendar.component.css']
+    styleUrls: ['./calendar.component.css',
+    ]
 })
 
 export class CalendarComponent {
@@ -18,11 +20,10 @@ export class CalendarComponent {
         initialView: 'dayGridMonth',
         plugins: [dayGridPlugin, interactionPlugin],
         dateClick: (arg) =>this.handleDateClick(arg),
-        events: [
-          { title: 'event 1', date: '2023-06-01' },
-          { title: 'event 2', date: '2023-06-02' },
-        ]
+        events: []
       };
+
+      eventsPromise: Promise<EventInput[]> = Promise.resolve([]);
 
       handleDateClick(arg: any) {
         alert('date click! ' + arg.dateStr);
