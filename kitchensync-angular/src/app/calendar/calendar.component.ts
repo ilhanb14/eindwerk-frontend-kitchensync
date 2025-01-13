fec82486bf82fb92d13dc063c3ed736c63fa5335
Merlinruy <ruysen.merlijn@gmail.com>
2025-01-13 11:56:14 +0100

    test for calendar

--- kitchensync-angular/src/app/calendar/calendar.component.ts
+++ kitchensync-angular/src/app/calendar/calendar.component.ts
@@ -1,32 +1,48 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CalendarEvent, CalendarView } from 'angular-calendar';
-import { startOfMonth, endOfMonth } from 'date-fns';
-import { CalendarModule } from 'angular-calendar';
-
+import { Component, Component, Signal } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import {FullCalendarModule} from "@fullcallendar/angular";
+import {calendaroptions} from "@fullcallendar/core";
+import daygridPlugin from "@fullcallendar/daygrid";
+import interactionplugin from "@fullcallendar/interaction";
+import timegridPlugin from "@fullcallendar/timegrid";
+import listplugin from "@fullcallendar/list";
 @Component({
-  selector: 'app-calendar',
-  standalone: true,
-  imports: [CalendarModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+    selector: 'app-calendar',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, FullCalendarModule],
+    templateUrl: './calendar.component.html',
+    styleUrls: ['./calendar.component.css']
 })
 
 export class CalendarComponent {
-  view: CalendarView = CalendarView.Month;
-  viewDate: Date = new Date();
-  events: CalendarEvent[] = [
-    {
-      start: startOfMonth(new Date()),
-      end: endOfMonth(new Date()),
-      title: 'Sample Event',
-      color: {
-        primary: '#ad2121',
-        secondary: '#FAE3E3'
-      },
-      actions: [],
-    }
-  ];
-activeDayIsOpen: boolean = true;
-calendarHeader: any;
+    calendarvisible = true;
+    calendaroptions = signal<calendaroptions>({
+        plugins: [
+            daygridPlugin,
+            interactionplugin,
+            timegridPlugin,
+            listplugin,
+        ],
+        headertoolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timelistWeek',
+        },
+        initialView: 'dayGridMonth',
+        initialEvents: INITIAL_EVENTS,
+        weekends: true,
+        editable: true,
+        selectable: true,  
+        selectMirror: true,
+        dayMaxEvents: true,
+        select: this.handleDateSelect.bind(this),
+        eventClick: this.handleEventClick.bind(this),
+        eventDrop: this.handleEventDrop.bind(this),
+    });
+    
+    handleCalendartoggle() {
+        this.calendaroptions.update((bool) => !bool);
+        }
+        
 }
