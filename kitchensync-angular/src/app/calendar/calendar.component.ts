8a81030e08491a2298ebae3c5a437b5c0bbad29c
Merlinruy <ruysen.merlijn@gmail.com>
2025-01-13 14:10:05 +0100

    added working calendar

--- kitchensync-angular/src/app/calendar/calendar.component.ts
+++ kitchensync-angular/src/app/calendar/calendar.component.ts
@@ -1,18 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions, EventInput } from '@fullcalendar/core'; 
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
 @Component({
     selector: 'app-calendar',
     standalone: true,
-    imports: [CommonModule, FullCalendarModule],
+    imports: [CommonModule, FullCalendarModule,],
     templateUrl: './calendar.component.html',
     styleUrls: ['./calendar.component.css',
-    ]
+    ],
 })
 
 export class CalendarComponent {
@@ -24,6 +22,9 @@ export class CalendarComponent {
       };
 
       eventsPromise: Promise<EventInput[]> = Promise.resolve([]);
+dayGridPlugin: any;
+interactionPlugin: any;
+events: any;
 
       handleDateClick(arg: any) {
         alert('date click! ' + arg.dateStr);
