fa5d250b427ae1bd21edc2ad6c370f9e9e6e733b
Merlinruy <ruysen.merlijn@gmail.com>
2025-01-13 13:31:10 +0100

    added stylesheets for calendar

--- kitchensync-angular/src/app/app.component.ts
+++ kitchensync-angular/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { CalendarComponent } from "./calendar/calendar.component";
+//import { CalendarComponent } from "./calendar/calendar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CalendarComponent],
+  imports: [RouterOutlet, HeaderComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
