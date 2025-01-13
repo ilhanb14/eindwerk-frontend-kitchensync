94dea546c3c13833dde90ab771cf9f8ebf1cbf69
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-13 13:40:25 +0100

    getAllPreferences method in service

--- kitchensync-angular/src/app/shared/preferences.service.ts
+++ kitchensync-angular/src/app/shared/preferences.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PreferencesService {
+  private url = "http://127.0.0.1:8000/api/";
 
   constructor() { }
+
+  async getAllPreferences() {
+    try {
+      const response = await fetch(this.url + "preferences/");
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching all preferences:', error);
+      throw error;
+    }
+  }
 }
