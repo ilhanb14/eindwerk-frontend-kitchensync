3d918f845f80566f8f41f6b85f9c41b0ff444109
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-14 11:26:11 +0100

    functions for updating user preference data

--- kitchensync-angular/src/app/shared/preferences.service.ts
+++ kitchensync-angular/src/app/shared/preferences.service.ts
@@ -35,4 +35,42 @@ export class PreferencesService {
       console.error("Error fetching user preferences:", error);
     }
   }
+
+    async addUserPreference(userId: number, preferenceId: number, important: boolean = false) {
+      try {
+        await fetch(this.url + "preferenceuser", {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            user_id: userId,
+            preference_id: preferenceId,
+            important: important
+          })
+        });
+      } catch (error) {
+        console.error("Error adding user preference:", error);
+      }
+    }
+  
+    async deleteUserPreference(userId: number, preferenceId: number) {
+      try {
+        await fetch(this.url + "preferenceuser", {
+          method: 'DELETE'
+        });
+      } catch (error) {
+        console.error("Error deleting user preference:", error);
+      }
+    }
+  
+    async toggleImportant(userId: number, preferenceId: number) {
+      try {
+        await fetch(this.url + "preferenceuser/" + userId + "/" + preferenceId, {
+          method: 'PUT'
+        });
+      } catch(error) {
+        console.error("Error toggling important preference:", error);
+      }
+    }
 }
