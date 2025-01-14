d3fe34d4215413135f4fca695f36d3f56e7c55b9
Merlinruy <ruysen.merlijn@gmail.com>
2025-01-14 11:40:52 +0100

    added  buttons to switch view to month, week and day

--- kitchensync-angular/src/app/calendar/calendar.component.ts
+++ kitchensync-angular/src/app/calendar/calendar.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions, EventInput } from '@fullcalendar/core'; 
+import { FullCalendarModule, FullCalendarComponent } from '@fullcalendar/angular';
+import { Calendar, CalendarOptions, EventInput } from '@fullcalendar/core'; 
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 @Component({
     selector: 'app-calendar',
     standalone: true,
@@ -16,29 +16,19 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 export class CalendarComponent {
     calendarOptions: CalendarOptions = {
-        initialView: 'dayGridMonth',
-        plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-        dateClick: (arg) =>this.handleDateClick(arg),
-        events: [
-          {
-            title: 'event 1',
-            date: '2023-01-01'
-          },
-          {
-            title: 'event 2',
-            date: '2023-01-02'
-          }
-        ]
-      };
+      initialView: 'dayGridWeek',
+      dateClick:(arg)  =>this.handleDateClick(arg), 
+      headerToolbar : {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],  
 
+      events: []
+    
+      };
       handleDateClick(arg: any) {
-        alert('date click! ' + arg.dateStr);
+        alert('date click! ' + arg.dateStr)
       }
-      changeView(view: string) {
-        this.calendarOptions = {
-            ...this.calendarOptions,
-            initialView: view
-        }
-        
     }
-}
