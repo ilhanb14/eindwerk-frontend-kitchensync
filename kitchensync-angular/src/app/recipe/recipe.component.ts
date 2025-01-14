0144cf50c66f4f383285add0963d93d85afc0161
Karel Segers <karelsegers@outlook.com>
2025-01-14 09:32:58 +0100

    Added a new component for a page that shows extended info on a specific recipe

---
+++ kitchensync-angular/src/app/recipe/recipe.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../shared/recipe';
+import { SpoonacularService } from '../shared/spoonacular.service';
+
+@Component({
+  selector: 'app-recipe',
+  imports: [],
+  templateUrl: './recipe.component.html',
+  styleUrl: './recipe.component.css'
+})
+
+export class RecipeComponent implements OnInit {
+  id: string | undefined;
+  recipe: Recipe | undefined;
+
+  constructor(private route: ActivatedRoute, private spoonacularService: SpoonacularService) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      this.spoonacularService.getMealById(Number(this.id)).then((recipe) => {
+        this.recipe = recipe;
+        console.log(this.recipe);
+      });
+    })
+  }
+}
