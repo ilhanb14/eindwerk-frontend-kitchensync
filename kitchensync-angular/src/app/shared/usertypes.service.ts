79bf62b78281b06b46a902c1b281cc665f1ab334
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-14 15:46:10 +0100

    added getters

--- kitchensync-angular/src/app/shared/usertypes.service.ts
+++ kitchensync-angular/src/app/shared/usertypes.service.ts
@@ -9,10 +9,20 @@ export class UserTypesService {
   constructor() { }
 
   async getAll() {
-
+    try {
+      const response = await fetch(this.url + "usertypes");
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching user types:", error);
+    }
   }
 
-  async getOne() {
-    
+  async getOne(id: number) {
+    try {
+      const response = await fetch(this.url + "usertypes/" + id);
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching user type:", error);
+    }
   }
 }
