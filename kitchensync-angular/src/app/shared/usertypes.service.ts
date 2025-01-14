6f63a5a7fc818ced34616a78facfb485d4aadcce
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-14 15:42:30 +0100

    added user types service

---
+++ kitchensync-angular/src/app/shared/usertypes.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserTypesService {
+  private url = "http://127.0.0.1:8000/api/"
+
+  constructor() { }
+
+  async getAll() {
+
+  }
+
+  async getOne() {
+    
+  }
+}
