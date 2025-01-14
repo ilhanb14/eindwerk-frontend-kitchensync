96a095a0103c5da68a1520d620a3f189e726c06a
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-14 13:46:16 +0100

    basic fetches

--- kitchensync-angular/src/app/shared/families.service.ts
+++ kitchensync-angular/src/app/shared/families.service.ts
@@ -4,6 +4,58 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FamiliesService {
+  private url = "http://127.0.0.1:8000/api/"
 
   constructor() { }
+
+  async getById(id: number) {
+    try {
+      const response = await fetch(this.url + "families/" + id);
+      return await response.json();
+    } catch(error) {
+      console.error("Error fetching family by id:", error);
+    }
+  }
+
+  async addFamily(familyName: string) {
+    try {
+      await fetch(this.url + "families", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: familyName
+        })
+      });
+    } catch(error) {
+      console.error("Error adding family:", error);
+    }
+  }
+
+  async updateFamily(familyId: number, familyName: string) {
+    try {
+      await fetch(this.url + "families/" + familyId, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: familyName
+        })
+      });
+    } catch(error) {
+      console.error("Error updating family:", error);
+    }
+  }
+
+  async deleteFamily(familyId: number) {
+    try {
+      await fetch(this.url + "families/" + familyId, {
+        method: 'DELETE'
+      })
+    } catch(error) {
+      console.error("Error deleting family:", error);
+    }
+  }
 }
