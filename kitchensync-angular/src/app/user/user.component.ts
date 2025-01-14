1610acc3b8de9c752ca3e923c63b744429f27b9d
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-14 15:36:24 +0100

    user component base

--- kitchensync-angular/src/app/user/user.component.ts
+++ kitchensync-angular/src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { PreferencesComponent } from "../preferences/preferences.component";
+import { UsersService } from '../shared/users.service';
+import { FamiliesService } from '../shared/families.service';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +10,24 @@ import { PreferencesComponent } from "../preferences/preferences.component";
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  private firstName: string = "";
+  private lastName: string = "";
+  private familyName: string = "";
+  private userType: string = "";
+  private email: string = "";
+  private family: any[] = [];
 
+  constructor(private usersService: UsersService, private familiesService: FamiliesService) {
+    this.fetchUserData();
+    this.fetchFamily();
+  }
+
+  async fetchUserData() {
+    // TODO fetch and display user data
+  }
+
+  async fetchFamily() {
+    // TODO fetch and display names of family members
+    // TODO link to family page
+  }
 }
