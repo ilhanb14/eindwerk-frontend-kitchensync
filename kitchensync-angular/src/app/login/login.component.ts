42c541f2a369f0bc4ae2f5a33abc7fe5f58a2a23
litchitsme <karelsegers@outlook.com>
2025-01-15 10:42:37 +0100

    Changed the login logic to onSubmit. Added the logout function

--- kitchensync-angular/src/app/login/login.component.ts
+++ kitchensync-angular/src/app/login/login.component.ts
@@ -1,23 +1,46 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../shared/login.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
+  providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  loginInfo = {};
+  emailInput!: string;
+  passwordInput!: string;
 
-  constructor(private loginService: LoginService) { }
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) { }
 
-  login(email: string, password: string) {
-    console.log(email, password);
-    this.loginService.login(email, password).then(res => {
-      console.log(res);
-    });
+  async onSubmit() {
+    const data = await this.loginService.login(this.emailInput, this.passwordInput);
+    if (data) {
+      localStorage.setItem('token', data.remember_token);
+
+      //Get the redirect URL from localStorage or use a default value
+      const redirectUrl = localStorage.getItem('redirectUrl') || '/home';
+      localStorage.removeItem('redirectUrl'); // Clean up
+
+      // Navigate to the protected route or home
+      this.router.navigate([redirectUrl]);
+    } else {
+      alert('Invalid username or password');
+    };
   }
 
+  logout() {
+    this.loginService.logout(localStorage.getItem('token') || '');
 
+    localStorage.removeItem('token');
+
+    this.router.navigate(['/login']);
+  }
 }
