efc8ded1419799eabba2c627e4ba4fd215e5c56a
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-15 14:33:05 +0100

    added unlike button

--- kitchensync-angular/src/app/likedmeals/likedmeals.component.ts
+++ kitchensync-angular/src/app/likedmeals/likedmeals.component.ts
@@ -24,7 +24,17 @@ export class LikedMealsComponent {
       // TODO use spoonacular
       //this.likedMeals.push(await this.spoonacularService.getMealById(mealId, false));
       // TEMPORARILY NOT USING SPOONACULAR TO AVOID USING POINTS
-      this.likedMeals.push({title: mealId, image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1280px-Placeholder_view_vector.svg.png"})
+      this.likedMeals.push({id: mealId, title: mealId, image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1280px-Placeholder_view_vector.svg.png"})
+    }
+  }
+
+  unlikeClicked(mealId: number) {
+    const mealName = this.likedMeals.find(meal => meal.id === mealId)!.title;
+    let confirmed = confirm("Are you sure you want to unlike \"" + mealName + "\"?")
+    
+    if(confirmed) {
+      this.likedMealsService.deleteLikedMealByData(mealId, this.userId);
+      this.likedMeals = this.likedMeals.filter(meal => meal.id != mealId);  // Update local array of likedmeals to avoid another fetch
     }
   }
 }
