38e5d25ce4d984d14f4a0c841de4bbedd01b660d
Karel Segers <karelsegers@outlook.com>
2025-01-15 09:24:20 +0100

    Made the service login. Added a function to log in, log out, verify token and register

---
+++ kitchensync-angular/src/app/shared/login.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LoginService {
+  private url = "http://127.0.0.1:8000/api";
+
+  constructor() { }
+
+  async login(email: string, password: string) {
+    try {
+      const response = await fetch(`${this.url}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      // Check if the response is OK
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      localStorage.setItem('token', data.remember_token);
+      return data;
+    } catch (error) {
+      console.error('There was a problem with the login fetch operation:', error);
+    }
+
+  }
+
+  logout(token: string) {
+    return fetch(`${this.url}/logout`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ token })
+    });
+  }
+
+  register(email: string, password: string, first_name: string, last_name: string, family_id: number, user_type_id: number) {
+    return fetch(`${this.url}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password, first_name, last_name, family_id, user_type_id })
+    });
+  }
+
+  verify(token: string) {
+    return fetch(`${this.url}/verify-token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ token })
+    });
+  }
+}
