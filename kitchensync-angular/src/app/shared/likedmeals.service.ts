7037f75a7afea4a16e5c37fc39b99c97c1ccdd7a
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-15 14:03:36 +0100

    liked meals service

---
+++ kitchensync-angular/src/app/shared/likedmeals.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LikedMealsService {
+  private url = "http://127.0.0.1:8000/api/";
+
+  constructor() { }
+
+  async getByUser(userId: number) {
+    try {
+      const response = await fetch(this.url + "likedmeals/" + userId);
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching liked meals:", error);
+    }
+  }
+
+  async likeMeal(mealId: number, userId: number) {
+    try {
+      await fetch(this.url + "likedmeals", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          meal_id: mealId,
+          user_id: userId
+        })
+      });
+    } catch (error) {
+      console.error("Error liking meal:", error);
+    }
+  }
+
+  async deleteLikedMeal(id: number) {
+    try {
+      fetch(this.url + "likedmeals/" + id, {
+        method: 'DELETE'
+      });
+    } catch (error) {
+      console.error("Error deleting liked meal:", error);
+    }
+  }
+
+  async deleteLikedMealByData(mealId: number, userId: number) {
+    try {
+      const likedMeals: any[] = await this.getByUser(userId);
+      let likedMeal = likedMeals.find(row => row.meal_id === mealId && row.user_id === userId);
+      if (likedMeal) {
+        this.deleteLikedMeal(likedMeal.id);
+      }
+    } catch (error) {
+      console.error("Error deleting meal by mealId and userId:", error);
+    }
+  }
+}
