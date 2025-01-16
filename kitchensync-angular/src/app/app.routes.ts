e91f947b98e2d082a3681e86f43c2b7407697d4b
litchitsme <karelsegers@outlook.com>
2025-01-16 20:22:30 +0100

    recipe/:id somehow got removed from routes. Put it back

--- kitchensync-angular/src/app/app.routes.ts
+++ kitchensync-angular/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeComponent } from './recipe/recipe.component';
 import { UserComponent } from './user/user.component';
 
 import { authGuard } from './auth.guard';
@@ -10,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'recipes', component: RecipesComponent }, // Add canActivate: [authGuard]
+    { path: 'recipe/:id', component: RecipeComponent }, // Add canActivate: [authGuard]
     { path: 'calendar', component: CalendarComponent }, // Add canActivate: [authGuard]
     { path: 'user', component: UserComponent }, // Add canActivate: [authGuard]
     { path: 'login', component: LoginComponent },
