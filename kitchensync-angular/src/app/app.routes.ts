586add20edd6e3f91bd7f56b54137bd1213110e7
litchitsme <karelsegers@outlook.com>
2025-01-16 20:02:26 +0100

    Added comments to add AuthGuard to the right routes. Made '**' redirect to home

--- kitchensync-angular/src/app/app.routes.ts
+++ kitchensync-angular/src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { UserComponent } from './user/user.component';
 
+import { authGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'recipes', component: RecipesComponent },
-    { path: 'calendar', component: CalendarComponent },
-    { path: 'user', component: UserComponent },
+    { path: 'recipes', component: RecipesComponent }, // Add canActivate: [authGuard]
+    { path: 'calendar', component: CalendarComponent }, // Add canActivate: [authGuard]
+    { path: 'user', component: UserComponent }, // Add canActivate: [authGuard]
     { path: 'login', component: LoginComponent },
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: 'home' }
 ];
