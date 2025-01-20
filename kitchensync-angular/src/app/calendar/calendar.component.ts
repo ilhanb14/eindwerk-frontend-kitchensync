b794f420c8536622245526f78650d2d0b41205f8
Merlinruy <ruysen.merlijn@gmail.com>
2025-01-20 10:26:47 +0100

    added recipe search  box to calendar page.

--- kitchensync-angular/src/app/calendar/calendar.component.ts
+++ kitchensync-angular/src/app/calendar/calendar.component.ts
@@ -1,67 +1,123 @@
-import { Component, signal, ChangeDetectorRef } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FullCalendarModule, FullCalendarComponent } from '@fullcalendar/angular';
-import { Calendar, CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core'; 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
 import { SpoonacularService } from '../shared/spoonacular.service';
-import listplugin from '@fullcalendar/list';
-import { INITIAL_EVENTS } from './event-utils';
+import { FormsModule } from '@angular/forms';
+
+interface SpoonacularRecipe {
+  id: number;
+  title: string;
+  image: string;
+  readyInMinutes?: number;
+  servings?: number;
+}
+
 @Component({
-    selector: 'app-calendar',
-    standalone: true,
-    imports: [CommonModule, FullCalendarModule],
-    templateUrl: './calendar.component.html',
-    styleUrls: ['./calendar.component.css',
-    ],
+  selector: 'app-calendar',
+  standalone: true,
+  imports: [CommonModule, FullCalendarModule, FormsModule],
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css'],
 })
-
 export class CalendarComponent {
-    calendarOptions: CalendarOptions = {
-      initialView: 'dayGridMonth',
-      initialEvents: INITIAL_EVENTS,
-      headerToolbar : {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      selectable: true,
-      editable: true,
-      select: this.handleDateSelect.bind(this),
-      eventClick: this.handleEventClick.bind(this),
-      eventsSet: this.handleEvents.bind(this),
-      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],  
-      events: []
-      };
-      currentEvents = signal<EventApi[]>([]);
-      handleEventClick(clickInfo: EventClickArg) {
-        if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-          clickInfo.event.remove();
-        }
-      }
-      handleEvents(events: EventApi[]) {
-        this.currentEvents.set(events);
-      }
+  recipes: SpoonacularRecipe[] = [];
+  searchQuery: string = '';
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay',
+    },
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin],
+    selectable: true,
+    editable: true,
+    droppable: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    drop: this.handleDrop.bind(this),
+  };
 
-      constructor(private spoonacularService: SpoonacularService) { }
+  currentEvents = signal<EventApi[]>([]);
 
-      handleDateSelect(selectInfo: DateSelectArg) {
-        const title = prompt('Please enter a new title for your event');
-        const calendarApi = selectInfo.view.calendar;
-        calendarApi.unselect();
-        if (title) {
-          calendarApi.addEvent({
-            id: createEventId(),
-            title,
-            start: selectInfo.startStr,
-            end: selectInfo.endStr,
-            allDay: selectInfo.allDay
-          });
-        }
-      }
+  constructor(private spoonacularService: SpoonacularService) {}
+
+  async searchRecipes(query: string) {
+    if (!query.trim()) return;
+
+    try {
+      const response = await this.spoonacularService.getRecipes({
+        query: query,
+        number: 10,
+        addRecipeInformation: 'true',
+      });
+
+      this.recipes = response.results.map((recipe: any) => ({
+        id: recipe.id,
+        title: recipe.title,
+        image: recipe.image,
+        readyInMinutes: recipe.readyInMinutes,
+        servings: recipe.servings,
+      }));
+    } catch (error) {
+      console.error('Error searching recipes:', error);
     }
+  }
 
-function createEventId(): string | undefined {
-  throw new Error('Function not implemented.');
+  onDragStart(event: DragEvent, recipe: SpoonacularRecipe) {
+    if (event.dataTransfer) {
+      event.dataTransfer.setData('recipe', JSON.stringify(recipe));
+    }
+  }
+
+  handleDrop(dropInfo: any) {
+    const recipeData = dropInfo.draggedEl.getAttribute('data-recipe');
+    if (!recipeData) return;
+
+    const recipe: SpoonacularRecipe = JSON.parse(recipeData);
+    dropInfo.view.calendar.addEvent({
+      id: this.createEventId(),
+      title: recipe.title,
+      start: dropInfo.date,
+      allDay: true,
+      extendedProps: {
+        recipeId: recipe.id,
+        image: recipe.image,
+        readyInMinutes: recipe.readyInMinutes,
+        servings: recipe.servings,
+      },
+    });
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: this.createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'?`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  private createEventId(): string {
+    return Math.random().toString(36).substring(2) + Date.now().toString(36);
+  }
 }
