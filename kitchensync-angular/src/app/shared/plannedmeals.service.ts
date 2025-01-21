a8c4a1d23c48127f24b2edd01318551bac5d8b75
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-21 15:34:01 +0100

    getByFamily

--- kitchensync-angular/src/app/shared/plannedmeals.service.ts
+++ kitchensync-angular/src/app/shared/plannedmeals.service.ts
@@ -4,6 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PlannedMealsService {
+  private url = "http://127.0.0.1:8000/api/";
 
   constructor() { }
+
+  async getByFamily(familyId: number) {
+    try {
+      const response = await fetch(this.url + "plannedmeals/" + familyId);
+      return await response.json();
+    } catch (error) {
+      console.error("Error getting planned meals by family:", error);
+    }
+  }
 }
