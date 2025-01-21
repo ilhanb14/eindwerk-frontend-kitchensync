0902ac384be617c454e9c2ec029fc417a26e43c9
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-21 15:54:11 +0100

    post planned meal

--- kitchensync-angular/src/app/shared/plannedmeals.service.ts
+++ kitchensync-angular/src/app/shared/plannedmeals.service.ts
@@ -25,4 +25,25 @@ export class PlannedMealsService {
       console.error("Error getting planned meals by family:", error);
     }
   }
+
+  async post(mealId: number, familyId: number, date: Date, mealtimeId: number, servings: number) {
+    try {
+      const response = await fetch(this.url + "plannedmeals", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          meal_id: mealId,
+          family_id: familyId,
+          date: date.toISOString().split('T')[0],
+          mealtime_id: mealtimeId,
+          servings: servings
+        })
+      });
+      return await response.json();
+    } catch (error) {
+      console.error("Error posting planned meal:", error);
+    }
+  }
 }
