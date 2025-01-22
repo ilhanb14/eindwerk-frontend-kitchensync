4f630745b6204532d5109b1a5cc07e1ccdeddd95
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-22 15:04:52 +0100

    Link to recipe page in liked meals

--- kitchensync-angular/src/app/likedmeals/likedmeals.component.ts
+++ kitchensync-angular/src/app/likedmeals/likedmeals.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LikedMealsService } from '../shared/likedmeals.service';
 import { SpoonacularService } from '../shared/spoonacular.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-likedmeals',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './likedmeals.component.html',
   styleUrl: './likedmeals.component.css'
 })
