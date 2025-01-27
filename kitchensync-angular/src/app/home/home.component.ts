e351bbe4f45f782ab77d869f6f43d88080c7a981
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-27 11:00:33 +0100

    Random suggestion with placeholders

--- kitchensync-angular/src/app/home/home.component.ts
+++ kitchensync-angular/src/app/home/home.component.ts
@@ -8,5 +8,8 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  randomRecipes: any[] = [
+    {title:"Recipe 1", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1280px-Placeholder_view_vector.svg.png"},
+    {title:"Recipe 2", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1280px-Placeholder_view_vector.svg.png"},
+    {title:"Recipe 3", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1280px-Placeholder_view_vector.svg.png"}];
 }
