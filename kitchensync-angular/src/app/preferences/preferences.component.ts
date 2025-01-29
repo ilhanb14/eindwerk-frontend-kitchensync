52d038627af5cc13496bc7c2743e58407907ef41
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-01-29 14:18:25 +0100

    Comments preferences component

--- kitchensync-angular/src/app/preferences/preferences.component.ts
+++ kitchensync-angular/src/app/preferences/preferences.component.ts
@@ -16,20 +16,26 @@ export class PreferencesComponent {
     this.fetchPreferences();
   }
 
+  /**
+   * Get all preference options from db for display and get all userPreferences for current user to set default checkbox values
+   */
   async fetchPreferences() {
     // get preferences from db
     this.preferenceList = await this.preferencesService.getAllPreferences();
     // set options to current values from user_preference table
-    this.userPreferences = await this.preferencesService.getAllUserPreferences(this.userId);  // TODO: user id based on currently logged in user
+    this.userPreferences = await this.preferencesService.getAllUserPreferences(this.userId);
     this.setCheckboxValues();
   }
 
+  /**
+   * Set values of all checkboxes for preferences based on data in this.userPreferences
+   */
   setCheckboxValues() {
     // set checkboxes based on values in userPrefences
     for (const preference of this.preferenceList) {
       let prefCheckbox = document.getElementById('check-'+preference.id)! as HTMLInputElement; // checkbox for this preference
       let importantCheckbox = document.getElementById('important-'+preference.id)! as HTMLInputElement; // checkbox that marks this preference as important
-      let currentUserPreference = this.userPreferences.find(userPref => userPref.id === preference.id);
+      let currentUserPreference = this.userPreferences.find(userPref => userPref.id === preference.id); // userPreference if it already exists for this user
 
       // if user has this preference
       if (currentUserPreference) {
@@ -42,25 +48,32 @@ export class PreferencesComponent {
     }
   }
 
+  /**
+   * Save changes to db based on checkbox inputs
+   */
   async saveChanges() {
     for (const preference of this.preferenceList) {
       let prefCheckbox = document.getElementById('check-'+preference.id)! as HTMLInputElement; // checkbox for this preference
       let importantCheckbox = document.getElementById('important-'+preference.id)! as HTMLInputElement; // checkbox that marks this preference as important
-      let savedUserPreference = this.userPreferences.find(userPref => userPref.id === preference.id)
+      let savedUserPreference = this.userPreferences.find(userPref => userPref.id === preference.id)  // userPreference if it already exists for this user
 
 
       if (prefCheckbox.checked && !savedUserPreference) {  // if checked and not already saved
-        await this.preferencesService.addUserPreference(this.userId, preference.id, importantCheckbox.checked);
-      } else if (prefCheckbox.checked && savedUserPreference.pivot.important != importantCheckbox.checked) {  // if checked, already saved, important changed
-        await this.preferencesService.toggleImportant(this.userId, preference.id);
+        await this.preferencesService.addUserPreference(this.userId, preference.id, importantCheckbox.checked); // add new userPreference
       } else if (!prefCheckbox.checked && savedUserPreference) {  // if unchecked, but is saved
-        await this.preferencesService.deleteUserPreference(this.userId, preference.id);
+        await this.preferencesService.deleteUserPreference(this.userId, preference.id); // Remove from userPreferences
+      } else if (prefCheckbox.checked && savedUserPreference.pivot.important != importantCheckbox.checked) {  // if checked, already saved, important changed
+        await this.preferencesService.toggleImportant(this.userId, preference.id);  // toggle important on this userPreference
       }
     }
 
     this.userPreferences = await this.preferencesService.getAllUserPreferences(this.userId);
   }
 
+  /**
+   * Event when a preference checkbox is toggled, used to toggle if importantCheckbox is enabled
+   * @param preferenceId 
+   */
   preferenceToggled(preferenceId: number) {
     let prefCheckbox = document.getElementById('check-'+preferenceId)! as HTMLInputElement;
     let importantCheckbox = document.getElementById('important-'+preferenceId)! as HTMLInputElement; // checkbox that marks this preference as important
